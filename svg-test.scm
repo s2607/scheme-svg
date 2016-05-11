e1052d3a4cf368a5b130e6dd2df5b96c67325c2b
phreaker2600 <swwiley@gmail.com>
2016-05-11 03:46:54 +0000

    works\!

--- svg-test.scm
+++ svg-test.scm
@@ -1,6 +1,14 @@
 (define pi  3.1415926)
 
 (define (circle r t) (cons (* r (cos t)) (* r (sin t))))
+(define (translate v curve)
+  (lambda (t) (let ((p (curve t))) 
+    (cons 
+      (+ (car v) (cdr p))
+      (+ (cdr v) (cdr p))))))
 (define (test) 
-  ((new-graph "blue" 2 "test.svg") (lambda (t) (circle 10 t)) 0 (* 2 pi)))
+  ((new-graph "blue" 2 "test.svg")
+   (lambda (t) 
+     (transform (cons 9 -9) (circle 10 t)))
+   0 (* 2 pi)))
 (test)
