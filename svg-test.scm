526fb43845c6595cf1a5f421063d0344dbbc6b95
phreaker2600 <swwiley@gmail.com>
2016-05-11 03:08:30 +0000

    tiv accepts output

--- svg-test.scm
+++ svg-test.scm
@@ -2,4 +2,5 @@
 
 (define (circle r t) (cons (* r (cos t)) (* r (sin t))))
 (define (test) 
-  ((new-graph "black" 2 "test.svg") (lambda (t) (circle 1 t)) 0 (* 2 pi)))
+  ((new-graph "blue" 2 "test.svg") (lambda (t) (circle 10 t)) 0 (* 2 pi)))
+(test)
